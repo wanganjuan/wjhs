642b6beaf88eb5a34006e499b346bbd5792473d6
jingwang36 <jingwang36@iflytek.com>
2019-12-02 10:41:42 +0800

    2

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-const exec = require('child_process').exec;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -17,8 +16,8 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage("请问你现在的心情如何",'你说什么','我不知道','再见！').then((select) => {
-			exec(`open 'https://www.baidu.com/'`);
+		vscode.window.showInformationMessage("图聆需要你的登录",'sign').then((select) => {
+			vscode.env.openExternal(vscode.Uri.parse('https://www.baidu.com/'));
 		});
 	});
 	let disposable1 = vscode.commands.registerCommand('extension.ccc', () => {
